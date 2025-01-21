12026b6c6a3b5c82795d98aae194fafbbe5740de
강정화 <rkdwwjd5@gmail.com>
2025-01-21 22:50:30 +0900

    Updates

--- server/pages/_app.js
+++
@@ -1,1 +0,0 @@
-(()=>{var r={};r.id=636,r.ids=[636],r.modules={287:(r,e,s)=>{"use strict";s.r(e),s.d(e,{default:()=>u});var t=s(732);function u({Component:r,pageProps:e}){return(0,t.jsx)(r,{...e})}s(880)},880:()=>{},732:r=>{"use strict";r.exports=require("react/jsx-runtime")}};var e=require("../webpack-runtime.js");e.C(r);var s=e(e.s=287);module.exports=s})();
